625d625ff2854bdd0ed65a0b8e6429e99e9d955b
정승균 <printline@jeongseung-gyun-ui-MacBookPro.local>
2022-06-09 09:52:33 +0900

    스터디숙제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useReducer, useMemo, useCallback } from "react";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
+import useInputs from "./UseInputs";
+
+function countActiveUsers(users) {
+  console.log("활성 사용자.");
+  return users.filter((user) => user.active).length;
+}
+
+const initialState = {
+  users: [
+    {
+      id: 1,
+      username: "printline",
+      email: "test@naver.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "txt",
+      email: "txt@naver.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "asdf",
+      email: "asdf@naver.com",
+      active: false,
+    },
+  ],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE_USER":
+      return {
+        inputs: state.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      throw new Error("에러!!");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username: "",
+    email: "",
+  });
+  const { username, email } = form;
+  const nextId = useRef(state.users.length);
+  const { users } = state;
+
+  const onCreate = useCallback(
+    (e) => {
+      dispatch({
+        type: "CREATE_USER",
+        user: {
+          id: nextId.current,
+          username,
+          email,
+        },
+      });
+      nextId.current += 1;
+      reset();
+    },
+    [username, email, reset]
+  );
+
+  const onToggle = useCallback((id) => {
+    dispatch({
+      type: "TOGGLE_USER",
+      id,
+    });
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    dispatch({
+      type: "REMOVE_USER",
+      id,
+    });
+  });
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div>활성 사용자 수: {count}</div>
+    </>
   );
 }
 
